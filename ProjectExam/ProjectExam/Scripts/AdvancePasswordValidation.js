1f15bc3510d478220a586e3f6945c4a099b3aced
Wahyu Trisvianto <wahyu.trisvianto@gmail.com>
2017-06-28 13:40:54 +0700

    realtime password validation

---
+++ ProjectExam/ProjectExam/Scripts/AdvancePasswordValidation.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+
+    $('input[type=password]').keyup(function () {
+        var pswd = $(this).val();
+
+        //validate the length
+        if (pswd.length < 6) {
+            $('#length').removeClass('isValid').addClass('isInValid');
+        } else {
+            $('#length').removeClass('isInValid').addClass('isValid');
+        }
+
+        //validate letter
+        if (pswd.match(/[A-z]/)) {
+            $('#letter').removeClass('isInValid').addClass('isValid');
+        } else {
+            $('#letter').removeClass('isValid').addClass('isInValid');
+        }
+
+        //validate capital letter
+        if (pswd.match(/[A-Z]/)) {
+            $('#capital').removeClass('isInValid').addClass('isValid');
+        } else {
+            $('#capital').removeClass('isValid').addClass('isInValid');
+        }
+
+        //validate number
+        if (pswd.match(/\d/)) {
+            $('#number').removeClass('isInValid').addClass('isValid');
+        } else {
+            $('#number').removeClass('isValid').addClass('isInValid');
+        }
+
+        //validate space
+        if (pswd.match(/[^a-zA-Z0-9\-\/]/)) {
+            $('#space').removeClass('isInValid').addClass('isValid');
+        } else {
+            $('#space').removeClass('isValid').addClass('isInValid');
+        }
+
+    }).focus(function () {
+        $('#pswd_info').show();
+    }).blur(function () {
+        $('#pswd_info').hide();
+    });
+
+});
