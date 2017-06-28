bb36bfbf4dc45ca8f7203b139c6391906e89f0c5
Wahyu Trisvianto <wahyu.trisvianto@gmail.com>
2017-06-28 13:12:05 +0700

    register and login user using .net framework 4.5.2 template

---
+++ ProjectExam/ProjectExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectExam.Startup))]
+namespace ProjectExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
